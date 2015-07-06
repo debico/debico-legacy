0e2205286bd489dc7618f91a53318dc83d30e013
Ricardo Zanini <ricardozanini@gmail.com>
2015-07-05 22:16:57 -0300

    tentativa de #79

--- debico-model/src/main/java/br/com/debico/model/PartidaBase.java
+++ debico-model/src/main/java/br/com/debico/model/PartidaBase.java
@@ -13,6 +13,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.MappedSuperclass;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.persistence.Transient;
 
 import org.joda.time.DateTime;
 
@@ -25,89 +26,88 @@ import static com.google.common.base.Objects.equal;
 import static com.google.common.base.Preconditions.checkNotNull;
 
 @MappedSuperclass
-public class PartidaBase implements Serializable,
-		Comparable<PartidaBase> {
+public class PartidaBase implements Serializable, Comparable<PartidaBase> {
 
-	private static final long serialVersionUID = 5084233650725169459L;
+    private static final long serialVersionUID = 5084233650725169459L;
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "ID_PARTIDA")
-	private int id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID_PARTIDA")
+    private int id;
 
-	@Column(name = "DC_LOCAL", length = 255)
-	private String local;
+    @Column(name = "DC_LOCAL", length = 255)
+    private String local;
 
-	@Column(name = "DH_PARTIDA", nullable = true)
-	@Temporal(TemporalType.TIMESTAMP)
-	private Date dataHoraPartida;
+    @Column(name = "DH_PARTIDA", nullable = true)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date dataHoraPartida;
 
-	@ManyToOne(cascade = { (CascadeType.ALL) }, optional = false)
-	@JoinColumn(nullable = false, name = "ID_TIME_MANDANTE", unique = false)
-	private Time mandante;
+    @ManyToOne(cascade = { (CascadeType.ALL) }, optional = false)
+    @JoinColumn(nullable = false, name = "ID_TIME_MANDANTE", unique = false)
+    private Time mandante;
 
-	@ManyToOne(cascade = { (CascadeType.ALL) }, optional = false)
-	@JoinColumn(nullable = false, name = "ID_TIME_VISITANTE", unique = false)
-	private Time visitante;
-	
+    @ManyToOne(cascade = { (CascadeType.ALL) }, optional = false)
+    @JoinColumn(nullable = false, name = "ID_TIME_VISITANTE", unique = false)
+    private Time visitante;
+
     @ManyToOne(cascade = { (CascadeType.ALL) }, optional = true)
     @JoinColumn(nullable = true, name = "ID_PLACAR", unique = false)
     private Placar placar;
-	
+
     @Column(name = "IN_COMPUTADA", nullable = false)
     private boolean computadaCampeonato = false;
 
-	public PartidaBase() {
-		this.computadaCampeonato = false;
-	}
+    public PartidaBase() {
+        this.computadaCampeonato = false;
+    }
 
-	public PartidaBase(final Time mandante, final Time visitante) {
-		this();
-		checkNotNull(mandante);
-		checkNotNull(visitante);
-		this.mandante = mandante;
-		this.visitante = visitante;
-	}
+    public PartidaBase(final Time mandante, final Time visitante) {
+        this();
+        checkNotNull(mandante);
+        checkNotNull(visitante);
+        this.mandante = mandante;
+        this.visitante = visitante;
+    }
 
-	public int getId() {
-		return id;
-	}
+    public int getId() {
+        return id;
+    }
 
-	public void setId(int id) {
-		this.id = id;
-	}
+    public void setId(int id) {
+        this.id = id;
+    }
 
-	public String getLocal() {
-		return local;
-	}
+    public String getLocal() {
+        return local;
+    }
 
-	public Date getDataHoraPartida() {
-		return new DateTime(dataHoraPartida).toDate();
-	}
+    public Date getDataHoraPartida() {
+        return new DateTime(dataHoraPartida).toDate();
+    }
 
-	public Time getMandante() {
-		return mandante;
-	}
+    public Time getMandante() {
+        return mandante;
+    }
 
-	public Time getVisitante() {
-		return visitante;
-	}
+    public Time getVisitante() {
+        return visitante;
+    }
 
-	public void setDataHoraPartida(Date dataHoraPartida) {
-		this.dataHoraPartida = dataHoraPartida;
-	}
+    public void setDataHoraPartida(Date dataHoraPartida) {
+        this.dataHoraPartida = dataHoraPartida;
+    }
 
-	public void setLocal(String local) {
-		this.local = local;
-	}
+    public void setLocal(String local) {
+        this.local = local;
+    }
 
-	public void setMandante(Time mandante) {
-		this.mandante = mandante;
-	}
+    public void setMandante(Time mandante) {
+        this.mandante = mandante;
+    }
 
-	public void setVisitante(Time visitante) {
-		this.visitante = visitante;
-	}
+    public void setVisitante(Time visitante) {
+        this.visitante = visitante;
+    }
 
     public boolean isComputadaCampeonato() {
         return computadaCampeonato;
@@ -116,70 +116,76 @@ public class PartidaBase implements Serializable,
     public void setComputadaCampeonato(boolean computada) {
         this.computadaCampeonato = computada;
     }
-    
+
     public final void setPlacar(Placar placar) {
-    	if(this.placar != null && placar != null) {
-    		this.placar.setId(placar.getId());
-    		this.placar.setGolsMandante(placar.getGolsMandante());
-    		this.placar.setGolsVisitante(placar.getGolsVisitante());
-    	} else {
-    		this.placar = placar;
-    	}
+        if (this.placar != null && placar != null) {
+            this.placar.setId(placar.getId());
+            this.placar.setGolsMandante(placar.getGolsMandante());
+            this.placar.setGolsVisitante(placar.getGolsVisitante());
+        } else {
+            this.placar = placar;
+        }
     }
 
     public Placar getPlacar() {
         return placar;
     }
 
-	public int compareTo(PartidaBase o) {
-		return ComparisonChain.start()
-				.compare(this.dataHoraPartida, o.getDataHoraPartida())
-				.compare(this.mandante, o.getMandante()).result();
-	}
+    /**
+     * A partida já começou? Compara a data da partida com a data de agora.
+     * 
+     * @since 2.0.4
+     * @return
+     */
+    @Transient
+    public boolean isIniciada() {
+        return new DateTime(this.dataHoraPartida).isAfterNow();
+    }
 
-	@Override
-	public int hashCode() {
-		return Objects.hashCode(
-		        this.id, 
-		        this.local, 
-		        this.mandante, 
-		        this.visitante, 
-		        this.dataHoraPartida,
-		        this.computadaCampeonato);
-	}
-	
-	protected ToStringHelper toStringHelper() {
-	    return  Objects.toStringHelper(this)
+    public int compareTo(PartidaBase o) {
+        return ComparisonChain.start()
+                .compare(this.dataHoraPartida, o.getDataHoraPartida())
+                .compare(this.mandante, o.getMandante()).result();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(this.id, this.local, this.mandante,
+                this.visitante, this.dataHoraPartida, this.computadaCampeonato);
+    }
+
+    protected ToStringHelper toStringHelper() {
+        return Objects
+                .toStringHelper(this)
                 .add("em ", this.local)
                 .addValue(
                         String.format("%s X %s", this.mandante, this.visitante))
-                .addValue(this.placar)
-                .omitNullValues();
-	}
+                .addValue(this.placar).omitNullValues();
+    }
 
-	@Override
-	public String toString() {
-		return this.toStringHelper().toString();
-	}
+    @Override
+    public String toString() {
+        return this.toStringHelper().toString();
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (obj == null) {
-			return false;
-		}
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
 
-		if (obj == this) {
-			return true;
-		}
+        if (obj == this) {
+            return true;
+        }
 
-		if (obj.getClass() != getClass()) {
-			return false;
-		}
+        if (obj.getClass() != getClass()) {
+            return false;
+        }
 
-		PartidaBase that = (PartidaBase) obj;
+        PartidaBase that = (PartidaBase) obj;
 
-		return equal(this.id, that.getId())
-		        && equal(this.mandante, that.getMandante())
-				&& equal(this.visitante, that.getVisitante());
-	}
+        return equal(this.id, that.getId())
+                && equal(this.mandante, that.getMandante())
+                && equal(this.visitante, that.getVisitante());
+    }
 }
--- debico-ui/src/main/webapp/static/js/model.js
+++ debico-ui/src/main/webapp/static/js/model.js
@@ -1,40 +1,41 @@
 /**
  * Util para serializar os objetos para JSON.
  */
-function SerializeJSON() {	
+function SerializeJSON() {
 	var self = this;
-	
+
 	this.placar = function(model) {
-		return ko.toJSON(model, function(key, value){
-			//ref: http://stackoverflow.com/questions/12461037/knockout-serialization-with-ko-tojson-how-to-ignore-properties-that-are-null
-			if(key === "idPartida" || key === "isValido" || key === "existe") {
+		return ko.toJSON(model, function(key, value) {
+			// ref:
+			// http://stackoverflow.com/questions/12461037/knockout-serialization-with-ko-tojson-how-to-ignore-properties-that-are-null
+			if (key === "idPartida" || key === "isValido" || key === "existe") {
 				return;
 			}
-			
+
 			return value;
 		});
 	};
-	
+
 	this.palpite = function(model) {
-		return ko.toJSON(model, function(key, value){
-			//o placar serializa
-			if(key === "placar") {
+		return ko.toJSON(model, function(key, value) {
+			// o placar serializa
+			if (key === "placar") {
 				return JSON.parse(self.placar(value));
 			}
-			
-			if(key === "pontos") {
+
+			if (key === "pontos") {
 				return;
 			}
-			
+
 			return value;
 		});
 	};
-	
+
 	this.partida = function(model) {
 		// o processo eh o mesmo, ambos possuem 'placar'
 		return self.palpite(model);
 	};
-	
+
 }
 
 /**
@@ -42,15 +43,15 @@ function SerializeJSON() {
  */
 function Placar(json, idPartida) {
 	var self = this;
-	
+
 	self.idPartida = idPartida;
 	self.id = "";
 
 	if (json) {
 		self.id = json.id;
-		
-		//eh observable ?
-		if(typeof json.golsMandante == "function") {
+
+		// eh observable ?
+		if (typeof json.golsMandante == "function") {
 			self.golsMandante = json.golsMandante;
 			self.golsVisitante = json.golsVisitante;
 		} else {
@@ -61,19 +62,20 @@ function Placar(json, idPartida) {
 		self.golsMandante = ko.observable("");
 		self.golsVisitante = ko.observable("");
 	}
-	
+
 	self.zerarPlacar = function() {
 		self.golsMandante("");
 		self.golsVisitante("");
-	};	
-	
+	};
+
 	self.formatar = function() {
 		return self.golsMandante() + " x " + self.golsVisitante();
 	}
-	
+
 	self.isValido = function() {
-		if(typeof self.golsMandante == "function") {
-			if (typeof self.golsVisitante() !== "undefined" && typeof self.golsMandante() !== "undefined") {
+		if (typeof self.golsMandante == "function") {
+			if (typeof self.golsVisitante() !== "undefined"
+					&& typeof self.golsMandante() !== "undefined") {
 				return (self.golsVisitante() !== "" && self.golsMandante() !== "");
 			} else {
 				return false;
@@ -101,10 +103,11 @@ function Time(json) {
 
 function Palpite(json) {
 	var self = this;
-	
+
 	self.id = (json) ? json.id : "";
 	self.idPartida = (json) ? json.idPartida : "";
-	self.placar = (json) ? (new Placar(json.placar, json.idPartida)) : (new Placar());
+	self.placar = (json) ? (new Placar(json.placar, json.idPartida))
+			: (new Placar());
 	self.pontos = (json) ? json.pontos : 0;
 }
 
@@ -113,19 +116,22 @@ function Palpite(json) {
  */
 function Partida(json) {
 	var self = this;
-	
+
 	// ref. http://momentjs.com/docs/#/displaying/
-	self.dataHoraPartida = ko.observable(moment(json.dataHoraPartida).format("DD/MM/YYYY HH:mm"));
-	self.dataHoraPartidaDisplay = moment(json.dataHoraPartida).format("ddd DD/MM/YYYY HH:mm");
+	self.dataHoraPartida = ko.observable(moment(json.dataHoraPartida).format(
+			"DD/MM/YYYY HH:mm"));
+	self.dataHoraPartidaDisplay = moment(json.dataHoraPartida).format(
+			"ddd DD/MM/YYYY HH:mm");
 	self.local = json.local;
 	self.mandante = new Time(json.mandante);
 	self.visitante = new Time(json.visitante);
 	self.computadaCampeonato = json.computadaCampeonato;
 	self.status = json.status;
 	self.id = json.id;
-	
+	self.iniciada = json.iniciada;
+
 	self.editing = ko.observable(false);
-	
+
 	self.edit = function() {
 		self.editing(true);
 	}
@@ -137,13 +143,16 @@ function Partida(json) {
 	}
 
 	self.hasPlacar = this.placar.isValido();
-	self.hasPlacarProcessado = ko.pureComputed(function() { return this.hasPlacar && this.computadaCampeonato }, this);
+	self.hasPlacarProcessado = ko.pureComputed(function() {
+		return this.hasPlacar && this.computadaCampeonato;
+	}, this);
 }
 
 function PartidaPalpite(json) {
 	Partida.call(this, json); // subclass
-	
-	this.palpite = (json.palpite !== null) ? (new Palpite(json.palpite)) : (new Palpite());
+
+	this.palpite = (json.palpite !== null) ? (new Palpite(json.palpite))
+			: (new Palpite());
 }
 
 // ref:
