21b9953011da50c2ee05f901ef2b08f09e1eef1f
ricardozanini <ricardozanini@gmail.com>
2015-07-21 11:10:49 -0300

    Acertos nos testes de unidade para #69

--- debico-resultados/src/main/java/br/com/debico/resultados/processors/ComputarPalpitesProcessor.java
+++ debico-resultados/src/main/java/br/com/debico/resultados/processors/ComputarPalpitesProcessor.java
@@ -5,7 +5,6 @@ import javax.inject.Named;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,28 +12,29 @@ import br.com.debico.bolao.brms.CalculoPalpitesService;
 import br.com.debico.resultados.Context;
 import br.com.debico.resultados.ProcessorBeans;
 
-@ActiveProfiles({"dev", "embedded-jpa"})
 @Named(ProcessorBeans.COMPUTA_PALPITES)
 @Transactional(readOnly = false, propagation = Propagation.MANDATORY)
 class ComputarPalpitesProcessor extends ProcessorSupport {
-    
-    private static final Logger LOGGER = LoggerFactory.getLogger(ComputarPalpitesProcessor.class);
+
+    private static final Logger LOGGER = LoggerFactory
+	    .getLogger(ComputarPalpitesProcessor.class);
 
     @Inject
     private CalculoPalpitesService calculoPalpitesService;
-    
+
     public ComputarPalpitesProcessor() {
-	
+
     }
 
     @Override
     public void execute(Context context) {
-	if(context.getPartidas().isEmpty()) {
+	if (context.getPartidas().isEmpty()) {
 	    LOGGER.debug("[execute] Nao ha partidas para computar palpites. Cancelando.");
 	    return;
 	}
-	
-	calculoPalpitesService.computarPalpites(context.getCampeonato(), context.getPartidas());
+
+	calculoPalpitesService.computarPalpites(context.getCampeonato(),
+		context.getPartidas());
 	this.executeNext(context);
     }
 
